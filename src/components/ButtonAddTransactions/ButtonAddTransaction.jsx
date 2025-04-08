0998c78508a579c7c2c391678d39a9d14eb04494
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-04-08 22:33:38 +0530

    Update ButtonAddTransaction.jsx

--- src/components/ButtonAddTransactions/ButtonAddTransaction.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransaction.jsx
@@ -1,20 +1,28 @@
 import SvgIcon from '@mui/material/SvgIcon';
 import css from './ButtonAddTransaction.module.css';
-export const ButtonAddTransaction = ({ onClick }) => {
+
+export const ButtonAddTransaction = () => {
+  const handleClick = () => {
+    alert("You're not authorised to do this action (admin function)");
+  };
+
   return (
-    <>
-      <button className={css.button} onClick={onClick}>
-        <SvgIcon
-          viewBox="0 0 24 24"
-          sx={{ width: '44px', height: '44px', padding: '0px' }}
-        >
-          <path d="M0 6h18v11H6V6z" fill="white" />
-          <path
-            fill="#24CCA7"
-            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"
-          />
-        </SvgIcon>
-      </button>
-    </>
+    <button
+      type="button"
+      className={css.button}
+      onClick={handleClick}
+      aria-label="Add transaction"
+    >
+      <SvgIcon
+        viewBox="0 0 24 24"
+        sx={{ width: '44px', height: '44px', padding: '0px' }}
+      >
+        <path d="M0 6h18v11H6V6z" fill="white" />
+        <path
+          fill="#24CCA7"
+          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"
+        />
+      </SvgIcon>
+    </button>
   );
 };
