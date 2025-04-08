663a1520cd32f37120b005b02865d46c2ab5b493
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-04-08 22:37:56 +0530

    Update DeleteButton.jsx

--- src/components/buttons/DeleteButton/DeleteButton.jsx
+++ src/components/buttons/DeleteButton/DeleteButton.jsx
@@ -1,11 +1,15 @@
-import css from './DeleteButton.module.css'
+import css from './DeleteButton.module.css';
 
-const DeleteButton = ({ onClick }) => {
+const DeleteButton = () => {
+  const handleClick = () => {
+    alert("You're not authorised to do this action (admin function)");
+  };
+
   return (
-    <>
-      <button className={css.deleteButton} type="button" onClick={onClick}>Delete</button>
-    </>
+    <button className={css.deleteButton} type="button" onClick={handleClick}>
+      Delete
+    </button>
   );
 };
 
-export default DeleteButton
+export default DeleteButton;
