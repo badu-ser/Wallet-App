e5577841bf4ca1c3422d197ef8744df5813415a0
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-04-13 01:25:37 +0530

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom';
 import Media from 'react-media';
 import { mediaQueries } from '../../utils/constants';
 import css from './Navigation.module.css';
-import CurrencyIcon from '../../assets/icons/currency.svg?react'; // Fixed path
 
 const Navigation = () => {
   const { mobile } = mediaQueries;
@@ -37,11 +36,11 @@ const Navigation = () => {
                 fill=""
               />
             </SvgIcon>
+
             <span className={css.span}>Home</span>
           </NavLink>
         </li>
-
-        <li className={css.listItem}>
+        <li>
           <NavLink className={css.link} to="statistics">
             <SvgIcon
               viewBox="0 0 38 38"
@@ -69,15 +68,14 @@ const Navigation = () => {
             <span className={css.span}>Statistics</span>
           </NavLink>
         </li>
-
-        <li className={css.listItem}>
+        <li>
           <Media queries={{ small: mobile }}>
             {matches =>
               matches.small ? (
                 <NavLink className={css.link} to="currency">
-                  <CurrencyIcon
+                  <SvgIcon
                     className={css.icon}
-                    color="inherit"
+                    viewBox="0 0 38 38"
                     sx={{
                       width: '44px',
                       height: '44px',
@@ -89,8 +87,15 @@ const Navigation = () => {
                         height: '24px',
                       },
                     }}
-                  />
-                  <span className={css.span}>Currency</span>
+                  >
+                    <path
+                      className={css.svg}
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M6 0C2.68629 0 0 2.68629 0 6V32C0 35.3137 2.68629 38 6 38H32C35.3137 38 38 35.3137 38 32V6C38 2.68629 35.3137 0 32 0H6ZM15.7523 15.5989C15.7523 16.7809 16.6607 17.5399 19.4852 18.2741C22.3096 19.0082 25.3332 20.2151 25.3332 23.7489C25.3332 26.2996 23.4046 27.7056 20.9783 28.166V30.8412H17.2455V28.1411C14.8565 27.631 12.8159 26.1005 12.6666 23.3756H15.4039C15.5408 24.8438 16.5487 25.9885 19.1119 25.9885C21.8617 25.9885 22.4714 24.6198 22.4714 23.7613C22.4714 22.6041 21.8493 21.5092 18.7386 20.7626C15.2671 19.9289 12.8905 18.498 12.8905 15.6238C12.8905 13.2223 14.8316 11.6546 17.2455 11.132V8.44434H20.9783V11.1693C23.5788 11.8039 24.8853 13.7698 24.9724 15.91H22.2225C22.1479 14.3546 21.3267 13.297 19.1119 13.297C17.0091 13.297 15.7523 14.2426 15.7523 15.5989Z"
+                      fill=""
+                    />
+                  </SvgIcon>
                 </NavLink>
               ) : null
             }
