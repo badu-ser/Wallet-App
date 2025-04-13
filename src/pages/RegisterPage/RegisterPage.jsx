d885b96c74996cff13cc15e3a31f3873698f71b9
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-04-14 01:43:52 +0530

    Update RegisterPage.jsx

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -6,7 +6,7 @@ const RegisterPage = () => {
     <div className={css.wrapper}>
       <div className={css.introWrapper}>
         <div className={css.img}></div>
-        <p className={css.text}>Finance App</p>
+        <p className={css.text}>X4 Wallet</p>
       </div>
       <div className={css.formWrapper}>
         <RegisterForm />
