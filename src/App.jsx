d8fdd0bd6a33f029162b365902711d244189f2a1
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-04-14 02:45:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,7 @@ const App = () => {
           <Route path="*" element={<PublicRoute component={<NotFound />} />} />
         </Route>
       </Routes>
+      <BrevoWidget/>
     </Suspense>
   );
 };
